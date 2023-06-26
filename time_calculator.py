7e246152812cc989cd7b2f24c51279ddf7fa34b0
Cehiim <116912049+Cehiim@users.noreply.github.com>
2023-06-26 20:30:54 -0300

    Update time_calculator.py

--- time_calculator.py
+++ time_calculator.py
@@ -1,46 +1,46 @@
 def add_time(start, duration, day = None):
   period = start.split()
   time = period[0].split(':')
-  total_min = int(time[1])
-  total_hr = int(time[0])
+  total_min = int(time[1]) # Guarda os minutos do horário inicial
+  total_hr = int(time[0]) # Guarda as horas do horário inicial
 
-  period = period[1]
+  period = period[1] # Guarda o período inicial
   periods = ('AM', 'PM')
-  pos = periods.index(period)
+  pos = periods.index(period) # Guarda a posição do período inicial
   
   time = duration.split(':')
-  total_min += int(time[1])
-  total_hr += int(time[0])
+  total_min += int(time[1]) # Soma os minutos a serem adicionados
+  total_hr += int(time[0]) # Soma as horas a serem adicionadas
 
   extra = 0
-  if(total_min >= 60):
+  if(total_min >= 60): # Caso o total de minutos seja maior ou igual a 60, separa os minutos adicionais e converte em horas numa variável auxiliar
     extra = total_min // 60
     total_min %= 60
 
-  total_hr += extra
+  total_hr += extra # É adicionado as horas extras
   extra = 0
-  if(total_hr >= 12):
+  if(total_hr >= 12): # Caso o total de horas seja maior ou igual a 12, separa as horas adicionais e converte em períodos numa variável auxiliar
     extra = total_hr // 12
     total_hr %= 12
   if(total_hr == 0):
     total_hr = 12
 
-  pos += extra
+  pos += extra # Acrescenta os períodos adicionais
   extra = 0
-  if(pos > 1):
-    extra = pos // 2
+  if(pos > 1): # Caso a posição do período final seja maior que o tamanho da tupla de períodos, guarda o período final e converte os períodos adionais em dias
+    extra = pos // 2 # Guarda a quantia de dias adiocionais
     pos %= 1
-  period = periods[pos]
+  period = periods[pos] # Guarda o período final
 
   if(day != None):
-    day = day.capitalize()
+    day = day.capitalize() # Formata o input do dia
     days = ('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday')
     if(day in days):
-      pos = days.index(day)
-      pos += extra
+      pos = days.index(day) # Guarda a posição do dia inicial
+      pos += extra # Acrescenta os dias adiocionais
       if(pos > 6):
         pos %= 7
-      day = days[pos]
+      day = days[pos] # Guarda o dia final
   
   if(extra == 0):
     extra = ''
