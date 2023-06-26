0dc21cd19165ff087cae236f38b676ebe74ae55f
Cehiim <116912049+Cehiim@users.noreply.github.com>
2023-06-26 20:55:03 -0300

    Update arithmetic_arranger.py

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -1,25 +1,25 @@
 def arithmetic_arranger(problems, solve = False):
-  size = len(problems)
-  limit = 5
-  op = ('+','-')
-  max_digits = 4
+  size = len(problems) # Guarda a quantia de problemas
+  limit = 5 # Limite do número de problemas
+  op = ('+','-') # Operações válidas
+  max_digits = 4 # Quantia máxima de dígitos presentes em um número
   
   if(size > limit):
     return 'Error: Too many problems.'
   else:
-    total_digits = size * [0]
-    digits_1 = size * [0]
-    digits_2 = size * [0]
+    total_digits = size * [0] # Guarda o número de digitos dos resultados de todos os problemas
+    digits_1 = size * [0] # Guarda o número de digitos dos primeiros operandos de todos os problemas
+    digits_2 = size * [0] # Guarda o número de digitos dos segundos operandos de todos os problemas
     for count in range(size):
       problems[count] = problems[count].split()
-      if(problems[count][1] not in op):
+      if(problems[count][1] not in op): # Verifica se a operação do problema é válida
         return "Error: Operator must be '+' or '-'."
-      elif(not problems[count][0].isnumeric() or not problems[count][2].isnumeric()):
+      elif(not problems[count][0].isnumeric() or not problems[count][2].isnumeric()): # Verifica se os operandos estão em formato numérico
         return 'Error: Numbers must only contain digits.'
       else:
         digits_1[count] = len(problems[count][0])
         digits_2[count] = len(problems[count][2])
-        if(digits_1[count] > max_digits or digits_2[count] > max_digits):
+        if(digits_1[count] > max_digits or digits_2[count] > max_digits): # Verifica se a quantia de dígitos dos operandos não excede o limite
           return 'Error: Numbers cannot be more than four digits.'
         else:
           if(digits_1[count] > digits_2[count]):
@@ -28,24 +28,24 @@ def arithmetic_arranger(problems, solve = False):
             total_digits[count] += 2 + digits_2[count]
     
     lines = 3
-    if(solve == True):
+    if(solve == True): # Caso o resultado dos problemas seja requisitado, será adicionado mais uma linha no output
       lines += 1
       
-    arranged_problems = ''
+    arranged_problems = '' # Guarda o output da função
     for line in range(lines):
-      for count in range(0, size):
-        if(line == 0):
+      for count in range(0, size): # Repetição para cada problema
+        if(line == 0): # Na primeira linha é gerado o primeiro operando
           space = ' ' * (total_digits[count] - digits_1[count])
           arranged_problems += space + problems[count][0]
-        elif(line == 1):
+        elif(line == 1): # # Na segunda linha é gerado a operação ao lado do segundo operando
           space = ' ' * (total_digits[count] - digits_2[count] - 1)
           arranged_problems += problems[count][1] + space + problems[count][2]
-        elif(line == 2):
+        elif(line == 2): # Na terceira linha é gerado uma divisória
           arranged_problems += '-' * total_digits[count]
-        else:
-          if(problems[count][1] == op[0]):
+        else: # Caso haja uma quarta linha
+          if(problems[count][1] == op[0]): # Caso a operação seja '+', gera o resultado da soma
             answer = int(problems[count][0]) + int(problems[count][2])
-          else:
+          else: # Caso a operação seja '-', gera o resultado da subtração
             answer = int(problems[count][0]) - int(problems[count][2])
           space = ' ' * (total_digits[count] - len(str(answer)))
           arranged_problems += space + str(answer)
